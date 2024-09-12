d44ca9ed689942c0f9fcd3b243d0b58e2b45482f
Garda Arraniri <garda.arraniri@gmail.com>
2024-09-12 20:09:44 +0700

    `wip:` add some boilerplate code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
+import { Response, Request, Handler } from 'express'
 import { app } from './application/web'
+
+type ApiResponse<T> = {
+    message: string
+    data: T
+}
+
+type PagingResponse<T> = {}
+
+type UserResponse = {
+    name: string
+    age: number
+}
+
+app.get('/', (req: Request, res: Response) => {
+    return res.json({
+        message: 'Success',
+        data: [
+            {
+                name: 'Garda Arraniri',
+                age: 20,
+            },
+        ],
+    })
+})
 
 app.listen(8000, () => {
     console.log('Application running on port 8000')
